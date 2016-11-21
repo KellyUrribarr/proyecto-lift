10054f2570b94f4033aa79075b4acfb912787e78
kellyurribarr <kellyurribarri@gmail.com>
2016-11-21 13:05:17 -0300

    actualizacion

---
+++ js/country.js
@@ -0,0 +1,10 @@
+
+  //guardar
+   $('.listado').on('click', function(ev){
+   	var nombre_pais = $(this).find('span').html();
+   	window.localStorage.setItem('nombre_pais'),
+   	nombre_pais;
+	
+});
+
+   
--- js/js.js
+++ js/js.js
@@ -1,1 +1,8 @@
-js
+$(document).ready(function()) {
+	mapas = new GMaps({
+		div: '#map',
+		lat: -33.4188881,
+		lng:-70.6548659,
+		zoom: 10
+	})
+}
