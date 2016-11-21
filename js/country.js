853aca4c0d3fea902780e79e70192954b47247eb
kellyurribarr <kellyurribarri@gmail.com>
2016-11-21 13:17:29 -0300

    comentario

--- js/country.js
+++ js/country.js
@@ -1,10 +1,9 @@
 
   //guardar
-   $('.listado').on('click', function(ev){
-   	var nombre_pais = $(this).find('span').html();
-   	window.localStorage.setItem('nombre_pais'),
-   	nombre_pais;
+   //$('.listado').on('click', function(ev){
+   //	var nombre_pais = $(this).find('span').html();
+   //	window.localStorage.setItem('nombre_pais',
+   //	nombre_pais);
 	
-});
+//});
 
-   
