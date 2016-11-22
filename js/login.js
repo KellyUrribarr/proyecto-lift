12d0ea55ac328d0f54659c8b807c051b45dbf9cc
kellyurribarr <kellyurribarri@gmail.com>
2016-11-22 10:53:35 -0300

    union de resgistro con country

---
+++ js/login.js
@@ -0,0 +1,15 @@
+$(document).on('ready', function(){
+  $('#listado li').on('click', function (){ // listado mi lista de origen (listado)
+    $(this).css('background', 'white'); // dandole color
+  });
+
+  $('#listado li').on('mouseover', function (){
+    $(this).css('background', '#F7F7F7');
+  });
+
+  $('#listado li').on('mouseout', function (){
+    $(this).css('background', 'white');
+  });
+});
+
+//--------------------------------------------------
