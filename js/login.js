6dea38f069864ca9301edc369cd0480eef5961a6
kelly urribarr <kellyurribarri@gmail.com>
2016-11-22 17:39:37 -0300

    enlaces con las banderas

--- js/login.js
+++ js/login.js
@@ -25,15 +25,98 @@ $(document).ready(function(){
 
    });
 });
-//-------------------
+//------------------ BANDERA AUSTRALIA
 $(document).ready(function(){ 
    $('#cambiar_australia').on('click',function(){
     var src = '32/GT.png';
     $('#cambio').attr('src', "32/AU.png");
-    //$('input, .form-control').text('+345');
-    $('input').attr('placeholder', "+4465");
+    $('input').attr('placeholder', "+61");
      $('#ocultar').toggle();
-    
-   
+
    });
 });
+
+//------------------ BANDERA ALEMANIA
+$(document).ready(function(){ 
+   $('#cambiar_alemania').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/DE.png");
+    $('input').attr('placeholder', "+49");
+     $('#ocultar').toggle();
+
+   });
+});
+
+//------------------ BANDERA ARGENTINA
+$(document).ready(function(){ 
+   $('#cambiar_argentina').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/GT.png");
+    $('input').attr('placeholder', "+54");
+     $('#ocultar').toggle();
+
+   });
+});
+
+//------------------ BANDERA CANADA
+$(document).ready(function(){ 
+   $('#cambiar_canada').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/CA.png");
+    $('input').attr('placeholder', "+1");
+     $('#ocultar').toggle();
+
+   });
+});
+
+
+//------------------ BANDERA COLOMBIA
+$(document).ready(function(){ 
+   $('#cambiar_colombia').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/CO.png");
+    $('input').attr('placeholder', "+67");
+     $('#ocultar').toggle();
+
+   });
+});
+
+
+//------------------ BANDERA CHILE
+$(document).ready(function(){ 
+   $('#cambiar_chile').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/CL.png");
+    $('input').attr('placeholder', "+56");
+     $('#ocultar').toggle();
+
+   });
+});
+
+
+//------------------ BANDERA URUGUAY
+$(document).ready(function(){ 
+   $('#cambiar_uruguay').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/UY.png");
+    $('input').attr('placeholder', "+598");
+     $('#ocultar').toggle();
+
+   });
+});
+
+
+//------------------ BANDERA VENEZUELA
+$(document).ready(function(){ 
+   $('#cambiar_venezuela').on('click',function(){
+    var src = '32/GT.png';
+    $('#cambio').attr('src', "32/VE.png");
+    $('input').attr('placeholder', "+58");
+     $('#ocultar').toggle();
+
+   });
+});
+
+
+
+
