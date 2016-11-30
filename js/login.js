d91d7a196995d77ddff0f9d868d29c9d6bef8032
kellyurribarr <kellyurribarri@gmail.com>
2016-11-30 10:28:28 -0300

    menu desplegable

--- js/login.js
+++ js/login.js
@@ -231,27 +231,26 @@ var contenido_apellido = $('#espacios_tres').val();
         }
         
 });
-
+ 
+ //------------
 $(document).ready(main);
- 
 var contador = 1;
- 
 function main(){
-  $('.menu_bar').click(function(){
-    // $('nav').toggle(); 
- 
-    if(contador == 1){
-      $('nav').animate({
-        left: '0'
-      });
-      contador = 0;
-    } else {
-      contador = 1;
-      $('nav').animate({
-        left: '-100%'
-      });
-    }
- 
-  });
- 
+    $('.verMenu').click(function(){
+        console.log(contador)
+
+        if(contador == 1){
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+
+    });
+
 };
