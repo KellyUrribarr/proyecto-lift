c81b55fd706447050311dda700f428360c6b1562
kellyurribarr <kellyurribarri@gmail.com>
2016-11-30 11:50:27 -0300

    actualizando

--- js/login.js
+++ js/login.js
@@ -254,3 +254,35 @@ function main(){
     });
 
 };
+
+//-------------
+$("#file-input").on( "click", function() {
+      $('.foto_usuario').hide(); //oculto mediante id
+    });
+ 
+
+$(window).load(function(){
+
+ $(function() {
+  $('#file-input').change(function(e) {
+      addImage(e); 
+     });
+
+     function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+    
+      if (!file.type.match(imageType))
+       return;
+  
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+     }
+  
+     function fileOnload(e) {
+      var result=e.target.result;
+      $('#imgSalida').attr("src",result);
+     }
+    });
+  });
