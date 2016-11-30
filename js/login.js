168876eeb4f702fa6032190a6c156c2553652654
kelly urribarr <kellyurribarri@gmail.com>
2016-11-30 00:31:00 -0300

    perfil

--- js/login.js
+++ js/login.js
@@ -230,9 +230,31 @@ var contenido_apellido = $('#espacios_tres').val();
           return false;
         }
         
-
-
 });
+
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main(){
+  $('.menu_bar').click(function(){
+    // $('nav').toggle(); 
+ 
+    if(contador == 1){
+      $('nav').animate({
+        left: '0'
+      });
+      contador = 0;
+    } else {
+      contador = 1;
+      $('nav').animate({
+        left: '-100%'
+      });
+    }
+ 
+  });
+ 
+};
 
 
 
